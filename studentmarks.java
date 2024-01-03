9a2c715e7cfcbbc41b13faa510dfad82f83704ef
u <u@DESKTOP-8P91KV8>
2024-01-03 15:37:34 +0530

    Nov2024

---
+++ studentmarks.java
@@ -0,0 +1,36 @@
+package operators;
+
+public class studentmarks {
+	
+	public static void main(String[] args) {
+		
+		int marks = 105; //Change the marks value here
+		
+		if (marks > 100) {
+			System.out.println("Invalid marks entered.");
+			
+		} else if (marks > 95) {
+			System.out.println("Grade: O");
+			
+		} else if (marks > 85) {
+			System.out.println("Grade: A");
+		
+		} else if (marks > 70) {
+			System.out.println("Grade: B");
+			
+		} else if (marks > 60 ) {
+			System.out.println("Grade: C");
+			
+		} else if (marks > 45) {
+			System.out.println("Grade: D");
+			
+		} else if (marks > 35) {
+			System.out.println("Grade: F");
+			
+			
+		}
+			
+		}
+			
+		}
+			
